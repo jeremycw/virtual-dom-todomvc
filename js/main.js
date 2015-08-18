75416fdf24f3856d256b3a7c526772ab75522f34
Jeremy Williams <jeremy@uken.com>
2015-08-18 12:31:07 -0400

    Don't use requestAnimationFrame

--- js/main.js
+++ js/main.js
@@ -8,10 +8,10 @@
   document.addEventListener("DOMContentLoaded", function() {
     var eventQueue = [];
 
-    document.body.addEventListener("click", function(e) { eventQueue.push(e); });
-    document.body.addEventListener("dblclick", function(e) { eventQueue.push(e); });
-    document.body.addEventListener("keydown", function(e) { eventQueue.push(e); });
-    window.addEventListener("hashchange", function(e) { eventQueue.push(e); });
+    document.body.addEventListener("click", update);
+    document.body.addEventListener("dblclick", update);
+    document.body.addEventListener("keydown", update);
+    window.addEventListener("hashchange", update);
 
     var view = window.location.hash.split("#")[1] || "/";
 
@@ -28,7 +28,7 @@
     var rootNode = createElement(tree);
     document.body.appendChild(rootNode);
 
-    function update() {
+    function update(ev) {
       function findTodo(target) {
         var id = parseInt(target.dataset.id);
         return _.find(state.todos, function(t) { return t.id === id });
@@ -45,73 +45,70 @@
         }
       }
 
-      while (eventQueue.length > 0) {
-        var ev = eventQueue.pop();
-        switch (ev.type) {
+      switch (ev.type) {
 
-          case "keydown":
-            if (ev.target.id === "todo-input") {
-              if (ev.key === "Enter" || ev.keyIdentifier === "Enter") {
-                var text = ev.target.value.trim();
-                if (text.length > 0) {
-                  state.todos.push({
-                    text: text,
-                    completed: false,
-                    editing: false,
-                    id: state.currentId
-                  });
-                  state.currentId++;
-                  state.input = "";
-                }
-              } else {
-                state.input = ev.target.value;
+        case "keydown":
+          if (ev.target.id === "todo-input") {
+            if (ev.key === "Enter" || ev.keyIdentifier === "Enter") {
+              var text = ev.target.value.trim();
+              if (text.length > 0) {
+                state.todos.push({
+                  text: text,
+                  completed: false,
+                  editing: false,
+                  id: state.currentId
+                });
+                state.currentId++;
+                state.input = "";
               }
-            } else if (ev.target.className === "edit") {
-              if (ev.key === "Enter" || ev.keyIdentifier === "Enter") {
-                var edit = _.find(state.todos, function(t) { return t.editing });
-                endEdit(edit);
-              } else {
-                var todo = findTodo(ev.target);
-                todo.text = ev.target.value;
-              }
+            } else {
+              state.input = ev.target.value;
             }
-            break;
-
-          case "click":
-            var id = parseInt(parseInt(ev.target.dataset.id));
-            var edit = _.find(state.todos, function(t) { return t.editing });
-
-            //cancel edit when you click anywhere except on the item you're editing
-            if (edit
-                && !(ev.target.className === "todo-label" && id === edit.id)
-                && !(ev.target.className === "edit" && id === edit.id))
-            {
+          } else if (ev.target.className === "edit") {
+            if (ev.key === "Enter" || ev.keyIdentifier === "Enter") {
+              var edit = _.find(state.todos, function(t) { return t.editing });
               endEdit(edit);
-            }
-
-            if (ev.target.className === "destroy") {
-              state.todos = _.reject(state.todos, function(t) { return t.id === id });
-            } else if (ev.target.className === "toggle") {
+            } else {
               var todo = findTodo(ev.target);
-              todo.completed = ev.target.checked;
-            } else if (ev.target.className === "toggle-all") {
-              var checked = ev.target.checked;
-              _.each(state.todos, function(t) { t.completed = checked; });
+              todo.text = ev.target.value;
             }
-            break;
+          }
+          break;
 
-          case "dblclick":
-            if (_.contains(ev.target.className.split(" "), "todo-label")) {
-              var todo = findTodo(ev.target);
-              todo.editing = true;
-            }
-            break;
+        case "click":
+          var id = parseInt(parseInt(ev.target.dataset.id));
+          var edit = _.find(state.todos, function(t) { return t.editing });
 
-          case "hashchange":
-            var url = ev.newURL.split("#")[1];
-            state.view = url;
-            break;
-        }
+          //cancel edit when you click anywhere except on the item you're editing
+          if (edit
+              && !(ev.target.className === "todo-label" && id === edit.id)
+              && !(ev.target.className === "edit" && id === edit.id))
+          {
+            endEdit(edit);
+          }
+
+          if (ev.target.className === "destroy") {
+            state.todos = _.reject(state.todos, function(t) { return t.id === id });
+          } else if (ev.target.className === "toggle") {
+            var todo = findTodo(ev.target);
+            todo.completed = ev.target.checked;
+          } else if (ev.target.className === "toggle-all") {
+            var checked = ev.target.checked;
+            _.each(state.todos, function(t) { t.completed = checked; });
+          }
+          break;
+
+        case "dblclick":
+          if (_.contains(ev.target.className.split(" "), "todo-label")) {
+            var todo = findTodo(ev.target);
+            todo.editing = true;
+          }
+          break;
+
+        case "hashchange":
+          var url = ev.newURL.split("#")[1];
+          state.view = url;
+          break;
       }
 
       localStorage.setItem('state', JSON.stringify(state));
@@ -120,9 +117,7 @@
       var patches = diff(tree, newTree);
       rootNode = patch(rootNode, patches);
       tree = newTree;
-      requestAnimationFrame(update);
     }
 
-    requestAnimationFrame(update);
   });
 })(window, document);
